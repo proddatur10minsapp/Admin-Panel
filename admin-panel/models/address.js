277f4944e9345d85ea5ada19a9ad92553f556228
proddatur10minsapp <proddatur10minsapp@gmail.com>
2025-05-17 14:38:11 +0530

    Update address.js

--- admin-panel/models/address.js
+++ admin-panel/models/address.js
@@ -1,16 +1,17 @@
 import mongoose from "mongoose";
 
 const addressSchema = new mongoose.Schema({
-  houseNo: { type: String, required: true },
-  streetName: { type: String, required: true },
-  city: { type: String, required: true },
-  state: { type: String, required: true },
-  postalCode: { type: String, required: true },
-  addressType: {
-    type: String,
-    required: true,
-    enum: ["work", "home", "school", "college"],
-  },
+  _id: { type: String }, 
+  type: { type: String },          
+  areaOrStreet: { type: String },  
+  landmark: { type: String },    
+  isDefault: { type: Boolean, default: false },
+  phoneNumber: { type: String, required: true },
+  _class: { type: String },       
+}, {
+  _id: false, 
 });
 
-export default addressSchema;
+const Address = mongoose.model("Address", addressSchema, "address");
+export default Address;
+
