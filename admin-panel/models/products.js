33a4b18a834404c065cc0a7ed579860e4c3200b1
proddatur10minsapp <proddatur10minsapp@gmail.com>
2025-05-22 10:17:18 +0530

    Update products.js

--- admin-panel/models/products.js
+++ admin-panel/models/products.js
@@ -13,10 +13,6 @@ const productSchema = new mongoose.Schema({
   keyFeatures: { type: String },
   specifications: { type: String },
   stock: { type: Number, required: true },
-  // ✅ These will be auto-added with defaults if not provided
-  isPresentInCart: { type: Boolean, default: false },
-  quantityInCart: { type: Number, default: 0 },
-  isPresentInWishList: { type: Boolean, default: false },
   category: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "Category",
